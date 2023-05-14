9423b6dfdcc135610da78422c82cca358da7f04f
Cristina Verdi <cristina.verdi@code-sherpas.rocks>
2023-05-14 19:01:23 +0200

    feat: implement featured posts card

--- components/layout/section/featured-posts/index.tsx
+++ components/layout/section/featured-posts/index.tsx
@@ -2,29 +2,27 @@ import SectionTitle from '@/components/layout/section/section-title'
 import { permanenetMarker, poppins } from '@/theme-config/fonts'
 import React, { ReactNode } from 'react'
 import { AiFillLinkedin, AiOutlineTwitter } from 'react-icons/all'
+import Image from 'next/image';
+
 export default function FeaturedPostsSection({children}): ReactNode {
   return (
     <>
-      <section role={'featured-posts'} className={'min-h-fit h-[20rem]'}>
+      <section role={'featured-posts'} className={'min-h-fit'}>
         {children}
-        <div role={'featured posts container'} className={`place-self-center max-w-[60%] bg-indigo-800`}>
-          <div role={'featured post'} className={'ml-40 w-[15rem]'}>
-              <div role={'featured post image'} className={`m-1 p-1`} >Image</div>
-              <h1 className={`m-1 p-1 text-l text-white ${permanenetMarker.className}`}>This is a post tile about em and rem in css</h1>
-              <h3 className={'m-1 p-1 text-l text-white'}>
+        <div role={'featured posts container'} className={`grid grid-flow-col auto-cols-max place-content-center`}>
+          <div role={'featured post'} className={'m-10 max-w-[22rem] bg-indigo-600 mx-6 m-h-fit rounded-xl'}>
+            <div className={`rounded-xl object-cover`}>
+              <img role={'featured post image'} src="/post-image.png" alt="post image" className={`rounded-t-xl`}/>
+              <h1 className={`px-7 py-2 text-xl text-white ${permanenetMarker.className} bg-indigo-400 `}>This is a post tile about em and rem in css</h1>
+              <h3 className={'px-7 py-2 text-l text-white bg-indigo-400'}>
                 Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
                 Lorem ipsum dolor sit amet.
               </h3>
-              <div role={'post-footer'} className={'mt-3 mt-1'}>
-                <button
-                  className={'m-1 p-2 border border-solid border-2 rounded-full'}>
-                  <AiOutlineTwitter size={'.5rem'} color={'white'}></AiOutlineTwitter>
-                </button>
-                <button className={'m-1 p-2 border border-solid border-2 rounded-full'}>
-                  <AiFillLinkedin size={'.5rem'} color={'white'}></AiFillLinkedin>
-                </button>
+              <div role={'footer'} className={`px-7 pb-5 pt-2 bg-indigo-400 rounded-b-xl`}>
+                <span className={'text-white font-medium'}>Read More</span>
               </div>
             </div>
+          </div>
         </div>
       </section>
     </>
--- components/layout/section/section-title.tsx
+++ components/layout/section/section-title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function SectionTitle() {
   return (
-    <div role={'section title'} className={'grid py-[4rem] place-items-center'}>
+    <div role={'section title'} className={'grid pt-[4rem] pb-[2rem] place-items-center'}>
       <h3 className={`my-1 p-1 text-5xl text-center ${permanenetMarker.className}`}>Featured Posts</h3>
       <h4 className={'my-1 p-1 max-w-[35rem] text-center'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h4>
     </div>
